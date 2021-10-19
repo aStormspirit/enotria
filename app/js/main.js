f200aa4f61d8e8b3037dfdff324e21fed7eb21bd
aStormspirit <shinkarenkovova@gmail.com>
2021-10-19 17:01:41 +0300

    'js-form'

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,6 @@
-function hello(){
-    console.log('Hello')
-}
+const form = document.querySelector('.login')
+
+form.addEventListener('click', ()=>{
+    const f = document.querySelector('.navbar__form')
+    f.classList.toggle('visible')
+})
--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ const autoprefixer = require('gulp-autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
 const del = require('del');
 const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+
 
 gulp.task('clean', async function(){
     del.sync('dist')
@@ -24,9 +26,20 @@ gulp.task('scss', function(){
     .pipe(browserSync.reload({stream: true}))
 })
 
+gulp.task('js', function(){
+  return gulp.src([
+    'node_modules/slick-carousel/slick/slick.js'
+  ])
+    .pipe(concat('libs.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('app/js'))
+    .pipe(browserSync.reload({stream: true}))
+});
+
 gulp.task('css', function(){
     return gulp.src([
-      'node_modules/normalize.css/normalize.css'
+      'node_modules/normalize.css/normalize.css',
+      'node_modules/slick-carousel/slick/slick.css'
     ])
       .pipe(concat('_libs.scss'))
       .pipe(gulp.dest('app/scss'))
@@ -76,4 +89,4 @@ gulp.task('browser-sync', function() {
 
   gulp.task('build', gulp.series('clean', 'export'))
 
-gulp.task('default', gulp.parallel('css','scss','browser-sync', 'watch'));
+gulp.task('default', gulp.parallel('css','scss','js','browser-sync', 'watch'));
